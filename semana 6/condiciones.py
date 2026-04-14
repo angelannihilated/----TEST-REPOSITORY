5cdcb1b642509f18a401afca234c24c0e3fb78dc
Daniel Alejandro Orellana González <daniel.orellana42@inacapmail.cl>
2026-04-14 12:21:48 -0400

    created a new file called test2 with the following content: * added a print statement to display a menu for a calculator * added an input statement to get the user's choice for the calculator operation * automated the calculator to perform the selected operation based on the user's input.

---
+++ semana 6/condiciones.py
@@ -0,0 +1,32 @@
+
+
+# menu
+# 10 productos
+# ingresa la plata
+# muestra los 10 productos
+# selecciona el producto que desea
+# realiza la compra y lo dispensa
+
+producto = 1, 2, 3
+
+valor1 = 2300
+valor2= 1000
+valor3 = 1200
+
+
+
+print("*** DISPENSADOR DE PRODUCTO ***")
+print("*** [!]SELECCIONA TU PRODUCTO[!] ***")
+
+print("[1] Monster Blanca SIN AZÚCAR 473ml // $2,300")
+print("[2] Barra Snickers // $1,000")
+print("[3] Papas fritas Lay's Ketchup // $1,200")
+
+producto = input("seleccione su producto: ")
+
+print ("usted ha seleccionado el producto: ", producto)
+print ("el valor del producto es de: ")
+
+
+
+print("andamo sin wea de plata a si que son gratis ahgjldshgjlsh chao ten tu producto qliao")
