cfc4c78c1dd011cbaa22da4bf6f1c3d8848de672
Daniel Alejandro Orellana González <daniel.orellana42@inacapmail.cl>
2026-04-21 10:45:30 -0400

    [+] ejercicio dos, la vdd no se que hice pero bueno lo hice en piloto automático ahjglsfhgjlsfhglfjshg
    mish

---
+++ semana 7/ola.py
@@ -0,0 +1,13 @@
+# sumar hasta un número n
+
+total = 10000
+contador = 0
+acumulador = 0
+
+for n in range(1, total + 1):
+    print(n)
+    contador = contador + 1
+    acumulador = n + 1
+    # como sumo cada n?
+
+print ("el contador es :", contador)
