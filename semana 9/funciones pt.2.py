53b16b1adb70b90d20510fb63e0d7585958ab396
Daniel Alejandro Orellana González <daniel.orellana42@inacapmail.cl>
2026-05-04 16:08:11 -0400

    +

---
+++ semana 9/funciones pt.2.py
@@ -0,0 +1,19 @@
+# funciones con retornos, entregan un valor al llamarlos
+# f(x) = x**2 + 2*x + 2
+
+valorUno = input("ingresa un número: ")
+
+# return
+def suma(a,b):
+    return a + b
+
+resultado = suma(10, 99)
+print(resultado)
+
+# solicita el nombre y apellido
+# crea un método que devuelve el nombre completo
+
+nombre_apellido = print("Cuál es su nombre y apellido?")
+nombre_apellido = input("Ingrese su nombre: ")
+
+print("Hola!", nombre_apellido, "!")
