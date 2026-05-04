c17cd82889ffdee12e7863a8ca87af2d3e4f00be
Daniel Alejandro Orellana González <daniel.orellana42@inacapmail.cl>
2026-05-04 15:50:03 -0400

    learning functions and all that on python #cool

---
+++ semana 9/funciones 04-05-26.py
@@ -0,0 +1,45 @@
+
+
+# funciones en python => metodos
+
+# funciones in retornos
+print("oal")
+# None // Null
+def escribir(palabra):
+    print(palabra)
+
+escribir("clases de eeeeeee")
+escribir("clases de eeeeeee")
+escribir("clases de eeeeeee")
+escribir("clases de eeeeeee")
+escribir("clases de eeeeeee")
+escribir("clases de eeeeeee")
+escribir("clases de eeeeeee")
+escribir("clases de eeeeeee")
+escribir("clases de eeeeeee")
+escribir("clases de eeeeeee")
+escribir("clases de eeeeeee")
+escribir("clases de eeeeeee")
+escribir("clases de eeeeeee")
+escribir("clases de eeeeeee")
+escribir("clases de eeeeeee")
+escribir("clases de eeeeeee")
+
+def resta():
+    print(str(2-2))
+
+resta()
+resta()
+resta()
+resta()
+resta()
+resta()
+resta()
+resta()
+
+
+
+# funciones con retornos, entregan un valor al llamarlos
+# f(x) = x**2 + 2*x + 2
+
+valorUno = input("ingresa un número: ")
