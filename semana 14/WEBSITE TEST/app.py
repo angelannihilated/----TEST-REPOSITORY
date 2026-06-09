19755b8f22d0ab3527ac927d3d86609057885f65
Daniel Alejandro Orellana González <daniel.orellana42@inacapmail.cl>
2026-06-08 22:23:39 -0400

    the code broke but i'll keep it here for the sake of documenting... if anyone wants to help me out with this that'll be cool :)!!

--- semana 14/WEBSITE TEST/app.py
+++ semana 14/WEBSITE TEST/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('sand.html')  # ← Serves HTML + static files automatically!
+    return render_template('/Users/inmari/Documents/GitHub/[+] TEST REPOSITORY/semana 14/WEBSITE TEST/sand.html')  # ← Serves HTML + static files automatically!
 
 if __name__ == '__main__':
     app.run(debug=True)  # Run locally at http://localhost:5000
